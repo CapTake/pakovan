f6d356f526bbf40dbe91f3a148a0ed01e77b6c1b
Boris Martirosov <boris.martirosov@gmail.com>
2021-03-04 15:42:10 +0300

    unpack added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-      Clear:
+    <h3>Clear text</h3>
       <div>
 
       <textarea class="tx" v-model.trim="svg"></textarea>
       </div>
       <button @click="pack">Pack</button>
       <div>
+      <h3>Gzip data(hex)</h3>
 
-      <textarea class="tx" readonly v-model="hex"></textarea>
+      <textarea class="tx" v-model="hex"></textarea>
       </div>
+      <button @click="unpack">Unpack</button>
       <div>
+      <h3>Verification</h3>
 
-      <textarea class="tx" readonly v-model="verify"></textarea>
+      <textarea class="tx" :class="{ok : isOk, err: !isOk}" readonly v-model="verify"></textarea>
       </div>
 
   </div>
@@ -33,20 +35,41 @@ export default {
       verify: ''
     }
   },
+  computed: {
+    isOk () {
+      return this.svg === this.verify
+    }
+  },
   methods: {
     pack () {
       let packed = pako.gzip(this.svg) // Uint8array
-      this.hex = packed.reduce((a, v) => a + ('0' + v.toString(16)).slice(-2), '0x')
+      let hex = packed.reduce((a, v) => a + ('0' + v.toString(16)).slice(-2), '')
+      this.hex = '0x' + hex
+      const bytes = new Uint8Array(hex.match(/.{1,2}/g).map(byte => parseInt(byte, 16)))
+      const dec = new TextDecoder('utf-8')
+      this.verify = dec.decode(pako.ungzip(bytes))
+    },
+    unpack () {
+      let packed = this.hex
+      if (packed.startsWith('0x')) packed = packed.substr(2)
+      const bytes = new Uint8Array(packed.match(/.{1,2}/g).map(byte => parseInt(byte, 16)))
+      const dec = new TextDecoder('utf-8')
+      this.svg = dec.decode(pako.ungzip(bytes))
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .tx {
   width: 600px;
   max-width: 100%;
   height: 200px;
+}
+.ok {
+    color: #008000;
+}
+.err {
+    color: #a00101;
 }
 </style>
